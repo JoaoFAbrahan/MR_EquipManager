9f2da06d01492faae211cb67788110d9a2dc4673
Joao Felipe Abrahan <joaofelipeabrahan@hotmail.com>
2021-08-17 18:04:09 -0400

    finalizado primeiro protótipo de tela das abas do DataGridView (Tela Armor)

--- View/Forms/F_Home/F_HomeWindow.Designer.cs
+++ View/Forms/F_Home/F_HomeWindow.Designer.cs
@@ -30,11 +30,6 @@ namespace View
         private void InitializeComponent()
         {
             this.TabLayerPanel_MAIN = new System.Windows.Forms.TableLayoutPanel();
-            this.GroupBox_Export = new System.Windows.Forms.GroupBox();
-            this.btn_Export = new System.Windows.Forms.Button();
-            this.Btn_BrowseExport = new System.Windows.Forms.Button();
-            this.lbl_SelectFile = new System.Windows.Forms.Label();
-            this.Tbx_FilePatchExport = new System.Windows.Forms.TextBox();
             this.lbl_TituloPagina = new System.Windows.Forms.Label();
             this.GroupBox_Import = new System.Windows.Forms.GroupBox();
             this.btn_Import = new System.Windows.Forms.Button();
@@ -42,7 +37,6 @@ namespace View
             this.lbl_SelectArchive = new System.Windows.Forms.Label();
             this.Tbx_FilePatchImport = new System.Windows.Forms.TextBox();
             this.TabLayerPanel_MAIN.SuspendLayout();
-            this.GroupBox_Export.SuspendLayout();
             this.GroupBox_Import.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -50,16 +44,14 @@ namespace View
             // 
             this.TabLayerPanel_MAIN.ColumnCount = 1;
             this.TabLayerPanel_MAIN.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
-            this.TabLayerPanel_MAIN.Controls.Add(this.GroupBox_Export, 0, 2);
             this.TabLayerPanel_MAIN.Controls.Add(this.lbl_TituloPagina, 0, 0);
             this.TabLayerPanel_MAIN.Controls.Add(this.GroupBox_Import, 0, 1);
             this.TabLayerPanel_MAIN.Dock = System.Windows.Forms.DockStyle.Fill;
             this.TabLayerPanel_MAIN.Location = new System.Drawing.Point(0, 0);
             this.TabLayerPanel_MAIN.Margin = new System.Windows.Forms.Padding(0);
             this.TabLayerPanel_MAIN.Name = "TabLayerPanel_MAIN";
-            this.TabLayerPanel_MAIN.RowCount = 3;
+            this.TabLayerPanel_MAIN.RowCount = 2;
             this.TabLayerPanel_MAIN.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 120F));
-            this.TabLayerPanel_MAIN.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.TabLayerPanel_MAIN.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
             this.TabLayerPanel_MAIN.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.TabLayerPanel_MAIN.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
@@ -67,78 +59,6 @@ namespace View
             this.TabLayerPanel_MAIN.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 20F));
             this.TabLayerPanel_MAIN.Size = new System.Drawing.Size(1166, 743);
             this.TabLayerPanel_MAIN.TabIndex = 0;
-            // 
-            // GroupBox_Export
-            // 
-            this.GroupBox_Export.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.GroupBox_Export.Controls.Add(this.btn_Export);
-            this.GroupBox_Export.Controls.Add(this.Btn_BrowseExport);
-            this.GroupBox_Export.Controls.Add(this.lbl_SelectFile);
-            this.GroupBox_Export.Controls.Add(this.Tbx_FilePatchExport);
-            this.GroupBox_Export.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.GroupBox_Export.Location = new System.Drawing.Point(100, 441);
-            this.GroupBox_Export.Margin = new System.Windows.Forms.Padding(100, 10, 100, 80);
-            this.GroupBox_Export.Name = "GroupBox_Export";
-            this.GroupBox_Export.Padding = new System.Windows.Forms.Padding(0);
-            this.GroupBox_Export.Size = new System.Drawing.Size(966, 171);
-            this.GroupBox_Export.TabIndex = 6;
-            this.GroupBox_Export.TabStop = false;
-            this.GroupBox_Export.Text = "Export JSON";
-            // 
-            // btn_Export
-            // 
-            this.btn_Export.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.btn_Export.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.btn_Export.Location = new System.Drawing.Point(433, 114);
-            this.btn_Export.Margin = new System.Windows.Forms.Padding(3, 3, 3, 20);
-            this.btn_Export.Name = "btn_Export";
-            this.btn_Export.Size = new System.Drawing.Size(96, 37);
-            this.btn_Export.TabIndex = 11;
-            this.btn_Export.Text = "Export";
-            this.btn_Export.UseVisualStyleBackColor = true;
-            this.btn_Export.Click += new System.EventHandler(this.btn_Export_Click);
-            // 
-            // Btn_BrowseExport
-            // 
-            this.Btn_BrowseExport.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
-            this.Btn_BrowseExport.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(121)))), ((int)(((byte)(20)))));
-            this.Btn_BrowseExport.FlatAppearance.BorderSize = 0;
-            this.Btn_BrowseExport.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.Btn_BrowseExport.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Btn_BrowseExport.ForeColor = System.Drawing.Color.White;
-            this.Btn_BrowseExport.Location = new System.Drawing.Point(773, 66);
-            this.Btn_BrowseExport.Margin = new System.Windows.Forms.Padding(0, 0, 50, 0);
-            this.Btn_BrowseExport.Name = "Btn_BrowseExport";
-            this.Btn_BrowseExport.Size = new System.Drawing.Size(119, 32);
-            this.Btn_BrowseExport.TabIndex = 11;
-            this.Btn_BrowseExport.Text = "Browse";
-            this.Btn_BrowseExport.UseVisualStyleBackColor = false;
-            this.Btn_BrowseExport.Click += new System.EventHandler(this.Btn_BrowseExport_Click);
-            // 
-            // lbl_SelectFile
-            // 
-            this.lbl_SelectFile.AutoSize = true;
-            this.lbl_SelectFile.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.lbl_SelectFile.Location = new System.Drawing.Point(50, 42);
-            this.lbl_SelectFile.Margin = new System.Windows.Forms.Padding(50, 0, 0, 1);
-            this.lbl_SelectFile.Name = "lbl_SelectFile";
-            this.lbl_SelectFile.Size = new System.Drawing.Size(79, 21);
-            this.lbl_SelectFile.TabIndex = 11;
-            this.lbl_SelectFile.Text = "Select file:";
-            // 
-            // Tbx_FilePatchExport
-            // 
-            this.Tbx_FilePatchExport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.Tbx_FilePatchExport.BackColor = System.Drawing.Color.White;
-            this.Tbx_FilePatchExport.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Tbx_FilePatchExport.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.Tbx_FilePatchExport.Location = new System.Drawing.Point(50, 66);
-            this.Tbx_FilePatchExport.Margin = new System.Windows.Forms.Padding(50, 0, 0, 0);
-            this.Tbx_FilePatchExport.Name = "Tbx_FilePatchExport";
-            this.Tbx_FilePatchExport.Size = new System.Drawing.Size(723, 32);
-            this.Tbx_FilePatchExport.TabIndex = 8;
             // 
             // lbl_TituloPagina
             // 
@@ -160,8 +80,8 @@ namespace View
             this.GroupBox_Import.Controls.Add(this.lbl_SelectArchive);
             this.GroupBox_Import.Controls.Add(this.Tbx_FilePatchImport);
             this.GroupBox_Import.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.GroupBox_Import.Location = new System.Drawing.Point(100, 200);
-            this.GroupBox_Import.Margin = new System.Windows.Forms.Padding(100, 80, 100, 0);
+            this.GroupBox_Import.Location = new System.Drawing.Point(100, 320);
+            this.GroupBox_Import.Margin = new System.Windows.Forms.Padding(100, 200, 100, 0);
             this.GroupBox_Import.Name = "GroupBox_Import";
             this.GroupBox_Import.Padding = new System.Windows.Forms.Padding(0);
             this.GroupBox_Import.Size = new System.Drawing.Size(966, 171);
@@ -215,8 +135,8 @@ namespace View
             this.Tbx_FilePatchImport.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
             this.Tbx_FilePatchImport.BackColor = System.Drawing.Color.White;
-            this.Tbx_FilePatchImport.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.Tbx_FilePatchImport.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Tbx_FilePatchImport.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Tbx_FilePatchImport.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.Tbx_FilePatchImport.Location = new System.Drawing.Point(50, 68);
             this.Tbx_FilePatchImport.Margin = new System.Windows.Forms.Padding(50, 0, 0, 0);
             this.Tbx_FilePatchImport.Name = "Tbx_FilePatchImport";
@@ -236,8 +156,6 @@ namespace View
             this.MinimizeBox = false;
             this.Name = "F_HomeWindow";
             this.TabLayerPanel_MAIN.ResumeLayout(false);
-            this.GroupBox_Export.ResumeLayout(false);
-            this.GroupBox_Export.PerformLayout();
             this.GroupBox_Import.ResumeLayout(false);
             this.GroupBox_Import.PerformLayout();
             this.ResumeLayout(false);
@@ -249,14 +167,9 @@ namespace View
         private System.Windows.Forms.TableLayoutPanel TabLayerPanel_MAIN;
         private System.Windows.Forms.Label lbl_TituloPagina;
         private System.Windows.Forms.GroupBox GroupBox_Import;
-        private System.Windows.Forms.GroupBox GroupBox_Export;
         private System.Windows.Forms.Button btn_Import;
         private System.Windows.Forms.Button Btn_BrowseImport;
         private System.Windows.Forms.Label lbl_SelectArchive;
         private System.Windows.Forms.TextBox Tbx_FilePatchImport;
-        private System.Windows.Forms.Button btn_Export;
-        private System.Windows.Forms.Button Btn_BrowseExport;
-        private System.Windows.Forms.Label lbl_SelectFile;
-        private System.Windows.Forms.TextBox Tbx_FilePatchExport;
     }
 }
